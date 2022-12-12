1e8360f0a80aa9e11f472e754cc1a3aa485b2544
Ilya Denisov <i.denisovs@gmail.com>
2022-12-12 18:00:24 +0200

    Removed unused import.

--- src/store/use-settings-store/index.ts
+++ src/store/use-settings-store/index.ts
@@ -1,5 +1,5 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
-import { ref, reactive } from 'vue';
 
 import ThemeStorage from './theme-storage';
 import LanguageStorage from './language-storage';
