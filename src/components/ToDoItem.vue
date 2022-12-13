780457d4260f5246775d48ec9157664f55b72461
Ilya Denisov <i.denisovs@gmail.com>
2022-12-13 05:15:59 +0200

    ToDo item got Edit mode.

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
+
     import { ToDo } from '../dtos';
 
     const props = defineProps<{
@@ -9,6 +11,12 @@
     defineEmits<{
         (e: 'remove', item: ToDo): void;
     }>();
+
+    const isEditMode = ref(false);
+
+    function update() {
+        isEditMode.value = false
+    }
 </script>
 
 <template>
@@ -16,11 +24,24 @@
         <v-card-text>
             <div class="d-flex align-center">
                 <v-checkbox density="compact" hide-details class="flex-grow-0 mr-4" color="primary" v-model="todo.completed"></v-checkbox>
-                <span class="flex-grow-1" :class="{ 'text-decoration-line-through': todo.completed }">
+                
+                <v-text-field 
+                    v-if="isEditMode" 
+                    density="compact" 
+                    variant="filled" 
+                    hide-details 
+                    single-line
+                    v-model="todo.title"
+                    @keyup.enter="update"
+                >
+                </v-text-field>
+                
+                <span class="flex-grow-1" :class="{ 'text-decoration-line-through': todo.completed }"  v-else>
                     {{todo.id}}. {{todo.title}}
                 </span>
+                
                 <span class="d-flex">
-                    <v-btn icon="edit" variant="plain" size="small" />
+                    <v-btn icon="edit" variant="plain" size="small" @click="isEditMode = !isEditMode" />
                     <v-btn icon="delete" variant="plain" size="small" @click="$emit('remove', todo)" />
                 </span>
             </div>
