c2ff51be083bbc17468210d13e938ca5785733a5
Ilya Denisov <i.denisovs@gmail.com>
2022-12-13 02:17:22 +0200

    Remove multiple selected todos .

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -24,18 +24,30 @@
             selectedTodos.value.splice(idx, 1);
         }
     }
+
+    function removeSelected() {
+        selectedTodos.value.forEach(todos.remove);
+        selectedTodos.value = [];
+    }
 </script>
 
 <template>
     <ToDoItem v-for="(item, index) in todos.list" 
+        :key="item.id"
         :idx="todos.list.length - index" 
         :todo="item" 
         @select="select"
         @deselect="deselect"
         @remove="remove"
     />
-    
-    <div>
-        Selected: {{selectedTodos.length}}
-    </div>
+
+    <v-btn block color="primary" class="delete-btn mt-10" v-if="selectedTodos.length > 1" @click="removeSelected">
+        Delete selected ({{selectedTodos.length}})
+    </v-btn>
 </template>
+
+<style scoped>
+.delete-btn {
+    height: 60px;
+}
+</style>
